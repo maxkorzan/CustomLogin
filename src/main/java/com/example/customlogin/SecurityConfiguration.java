619ae0ef7817e38a0b33ecfadb6e64f7fa293e12
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-03 13:36:59 -0500

    2nd day commit

--- src/main/java/com/example/customlogin/SecurityConfiguration.java
+++ src/main/java/com/example/customlogin/SecurityConfiguration.java
@@ -37,14 +37,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "h2-console/**", "/register").permitAll()
+                .antMatchers("/", "h2-console/**", "/register", "/css/**", "/js/**", "/vendor/**", "/images/**", "/fonts/**").permitAll()
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
 
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").permitAll().permitAll()
+                /*.logoutSuccessUrl("/login")*/.permitAll().permitAll()
                 .and()
                 .httpBasic();
                 http.csrf().disable();
